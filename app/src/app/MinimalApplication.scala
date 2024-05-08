0aec74d3556d963d0b6c9df02d75ff94c684eb3f
danielRamirezL <danielramirez@ug.uchile.cl>
2024-05-08 01:22:12 -0400

    es posible obtener un personaje de la lista, la lista completa, y atacar personajes con requests

---
+++ app/src/app/MinimalApplication.scala
@@ -0,0 +1,99 @@
+package app
+
+import cask.model.Response
+import ujson.Obj
+import model.Character
+
+object MinimalApplication extends cask.MainRoutes{
+  /* Qué es lo que quiero hacer:
+  - GET información de todos los personajes en juego
+  - POST actualización de la vida de un personaje en juego (no me deja con POST)
+  */
+
+  val dummy1: Character = new Character(1, "dummy1", 100, 10)
+  val dummy2: Character = new Character(2, "dummy2", 50, 5)
+  val dummy3: Character = new Character(3, "dummy3", 51, 6)
+  val dummy4: Character = new Character(4, "dummy4", 52, 7)
+  val dummy5: Character = new Character(5, "dummy5", 53, 8)
+
+  val characters: List[Character] = List(dummy1, dummy2, dummy3, dummy4, dummy5)
+
+  // Request -> Response
+  // An http server is basically a function that for every request that it receives, it
+  // will return a response, we call these functions "routes", and a server is nothing more
+  // than a set of these routes chained together.
+
+  @cask.get("/character")
+  def character(): Response[Obj] = {
+    val response = ujson.Obj(
+      "characters" -> ujson.Arr(
+        for (character <- characters) yield ujson.Obj(
+          "id" -> character.id,
+          "name" -> character.name,
+          "hp" -> character.hp,
+          "attack" -> character.attack
+        )
+      )
+    )
+    cask.Response(
+      response,
+      headers = Seq(
+        "Access-Control-Allow-Origin" -> "*",
+        "Content-Type" -> "application/json"
+      )
+    )
+  }
+
+  @cask.get("/character/:id")
+  def character(id: Int): Response[Obj] = {
+    for (character <- characters) {
+      if (character.id == id) {
+        val response = ujson.Obj(
+          "id" -> character.id,
+          "name" -> character.name,
+          "hp" -> character.hp,
+          "attack" -> character.attack
+        )
+        return cask.Response(
+          response,
+          headers = Seq(
+            "Access-Control-Allow-Origin" -> "*",
+            "Content-Type" -> "application/json"
+          )
+        )
+      }
+    }
+    cask.Response(
+      ujson.Obj("error" -> "Character not found"),
+      statusCode = 404,
+      headers = Seq(
+        "Access-Control-Allow-Origin" -> "*",
+        "Content-Type" -> "application/json"
+      )
+    )
+  }
+
+  @cask.get("/attack/:fromId/:toId")
+  def attack(fromId: Int, toId: Int): Response[Obj] = {
+    var attacker: Character = null
+    var receiver: Character = null
+    for (character <- characters) {
+      if (character.id == fromId) {
+        attacker = character
+      }
+      if (character.id == toId) {
+        receiver = character
+      }
+    }
+    receiver.hp -= attacker.attack
+    cask.Response(
+      ujson.Obj("message" -> s"${attacker.name} attacked ${receiver.name}"),
+      headers = Seq(
+        "Access-Control-Allow-Origin" -> "*",
+        "Content-Type" -> "application/json"
+      )
+    )
+  }
+
+  initialize()
+}
---
+++ app/src/model/Character.scala
@@ -0,0 +1,4 @@
+package model
+
+class Character(val id: Int, val name: String, var hp: Int, val attack: Int) {
+}
